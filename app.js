bffac0b9801fab8bc2a9d4cd4cf06cffee56a8a3
Andy <andyvelazquez13@yahoo.com>
2021-04-06 21:15:02 -0700

    add nav bar and add routing paths and add pages

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000; /*localhost:3000*/
 
 app.use(express.static("public")); /* directory to serve static files(
                                     where css, images, etc go) */
-app.set('view enginer', 'ejs'); /*passing specific configuration values to express */
+app.set('view engine', 'ejs'); /*passing specific configuration values to express */
 
 app.use(express.urlencoded({
   extended:true
@@ -15,7 +15,22 @@ app.use(express.urlencoded({
 
 app.get("/", function(req, res){
 
-  res.send("<h1>Hello World </h1>");
+  console.log("get request, /");
+  res.render("journal");
+
+});
+
+app.get("/about", function(req, res) {
+
+  console.log("get request , /about")
+  res.render("about")
+
+});
+
+app.get("/contact", function(req, res) {
+
+  console.log("get request, /contact")
+  res.render("contact")
 
 });
 
